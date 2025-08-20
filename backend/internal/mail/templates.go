17a000520d32f3beb2d576e294b45da4792cdd13
saravahdatipour <saravahdatipour@gmail.com>
2025-08-20 16:14:14 +0200

    Remove unused code

--- backend/internal/mail/templates.go
+++ backend/internal/mail/templates.go
@@ -3,8 +3,6 @@ package mail
 import (
 	"bytes"
 	"text/template"
-
-	gomail "gopkg.in/mail.v2"
 )
 
 func RenderHTMLtemplate(dir string, link string) (string, error) {
@@ -20,16 +18,5 @@ func RenderHTMLtemplate(dir string, link string) (string, error) {
 	}
 
 	return buf.String(), nil
-
-}
-
-func ComposeEmail(e Email) *gomail.Message {
-	gm := gomail.NewMessage()
-	gm.SetHeader("From", e.From)
-	gm.SetHeader("To", e.To)
-	gm.SetHeader("Subject", e.Subject)
-	gm.SetBody("text/html", e.Body)
-
-	return gm
 
 }
