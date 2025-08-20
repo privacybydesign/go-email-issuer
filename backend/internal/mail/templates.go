7c601f873437e2c2e0f1502e5adfc7e0671458d4
saravahdatipour <saravahdatipour@gmail.com>
2025-08-20 16:10:39 +0200

    Remove unnecessary param

--- backend/internal/mail/templates.go
+++ backend/internal/mail/templates.go
@@ -23,12 +23,12 @@ func RenderHTMLtemplate(dir string, link string) (string, error) {
 
 }
 
-func ComposeEmail(e Email, body string) *gomail.Message {
+func ComposeEmail(e Email) *gomail.Message {
 	gm := gomail.NewMessage()
 	gm.SetHeader("From", e.From)
 	gm.SetHeader("To", e.To)
 	gm.SetHeader("Subject", e.Subject)
-	gm.SetBody("text/html", body)
+	gm.SetBody("text/html", e.Body)
 
 	return gm
 
