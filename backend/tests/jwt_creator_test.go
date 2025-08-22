a30d732ca08c1abf6731a6969471d8608cb44a02
saravahdatipour <saravahdatipour@gmail.com>
2025-08-22 13:35:06 +0200

    Use require

--- backend/tests/jwt_creator_test.go
+++ backend/tests/jwt_creator_test.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"backend/internal/issue"
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreatingJwt(t *testing.T) {
