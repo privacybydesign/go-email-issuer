c1ace2106064c682fa82334bc7c283f7db36b960
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2026-01-15 11:52:36 +0100

    update token generator

--- backend/internal/core/token_gen.go
+++ backend/internal/core/token_gen.go
@@ -2,35 +2,65 @@ package core
 
 import (
 	"math/rand"
+	"sync"
+	"time"
 )
 
 type TokenGenerator interface {
 	GenerateToken() string
 }
 
-type RandomTokenGenerator struct{}
+type RandomTokenGenerator struct {
+	mu sync.Mutex
+	r  *rand.Rand
+}
 
 func NewRandomTokenGenerator() *RandomTokenGenerator {
-	return &RandomTokenGenerator{}
+	return &RandomTokenGenerator{
+		mu: sync.Mutex{},
+		r:  rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
 }
 
 func (tg *RandomTokenGenerator) GenerateToken() string {
-	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-	length := 6
+	const (
+		letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		digits  = "0123456789"
+		length  = 6
+	)
 	token := make([]byte, length)
-	for i := range token {
-		token[i] = charset[rand.Intn(len(charset))]
+
+	tg.mu.Lock()
+	defer tg.mu.Unlock()
+
+	// Anywhere between 2 and 6 digits
+	numDigits := 2 + tg.r.Intn(4)
+
+	// Add the digits first
+	for i := range numDigits {
+		token[i] = digits[tg.r.Intn(len(digits))]
 	}
+
+	// Fill remaining characters from full charset
+	const charset = letters + digits
+	for i := numDigits; i < length; i++ {
+		token[i] = charset[tg.r.Intn(len(charset))]
+	}
+
+	// Shuffle to avoid predictable digit positions
+	tg.r.Shuffle(len(token), func(i, j int) {
+		token[i], token[j] = token[j], token[i]
+	})
+
 	return string(token)
 }
 
 // for testing purposes it's useful to have a static token
 // in production the RandomTokenGenerator should always be used
 type StaticTokenGenerator struct {
-	Token string
+	token string
 }
 
 func (tg *StaticTokenGenerator) GenerateToken() string {
-	return tg.Token
+	return tg.token
 }
