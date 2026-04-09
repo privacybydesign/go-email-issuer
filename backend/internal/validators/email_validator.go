38db19ab7b375559e0c7a3a7cfd8a5bd8b276387
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2026-04-09 14:46:37 +0200

    make email validation case insensitive

--- backend/internal/validators/email_validator.go
+++ backend/internal/validators/email_validator.go
@@ -8,7 +8,8 @@ import (
 type EmailValidator struct{}
 
 // ParseAndValidateEmailAddress checks whether the provided input is valid.
-// It accepts RFC-5322 formatted emailaddresses (i.e. "John Doe <john.doe@example.com>"), but only in lowercase.
+// It accepts RFC-5322 formatted emailaddresses (i.e. "John Doe <john.doe@example.com>").
+// Any uppercase characters in the address are normalized to lowercase.
 // It returns a boolean indicating validity, a parsed emailaddress (if valid) or an error message key (if invalid).
 // If valid, the second return value contains the parsed address from the RFC-5322 format (e.g. "john.doe@example.com").
 // If invalid, an error message is returned in the third return value.
@@ -29,11 +30,7 @@ func (v *EmailValidator) ParseAndValidateEmailAddress(email string) (bool, *stri
 		err := "error_email_format"
 		return false, nil, &err
 	} else {
-		// We only accept lowercase emailaddresses
-		if strings.ToLower(addr.Address) != addr.Address {
-			err := "error_email_format_lowercase"
-			return false, nil, &err
-		}
-		return true, &addr.Address, nil
+		normalized := strings.ToLower(addr.Address)
+		return true, &normalized, nil
 	}
 }
